342058573a0e9df034c4b423f4fbf7b8d0bc6702
mika <mikadze.davit@gmail.com>
2026-02-13 20:16:38 +0400

    setup game engine

---
+++ apps/game-engine/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  moduleNameMapper: {
+    '^@engine/(.*)$': '<rootDir>/src/engine/$1',
+    '^@betting/(.*)$': '<rootDir>/src/betting/$1',
+    '^@rng/(.*)$': '<rootDir>/src/rng/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+  },
+};
